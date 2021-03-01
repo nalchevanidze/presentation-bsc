9329cee7d980a9eaf12dc6cebe320c7456b78818
Davit Nalchevanidze <d.nalchevanidze@gmail.com>
2021-03-01 18:51:59 +0100

    update

---
+++ src/code/records.hs
@@ -0,0 +1,16 @@
+data Deity = Deity { 
+  name :: Text,
+  age :: Int
+}
+
+deities :: [Deity]
+deities = [
+  Deity {
+    name = "Morpheus",
+    age = 25351124
+  },
+  Deity {
+    age = 3223413352
+    name = "Zeus",
+  }
+]
